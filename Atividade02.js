9b88146e27b10e11467535baef167db16e172ad6
Pablo Castro da Silva <91504069+silvapablo@users.noreply.github.com>
2021-10-15 23:08:12 -0400

    Add files via upload

---
+++ Atividade02.js
@@ -0,0 +1,24 @@
+let hoje = new Date()
+let data = new Date("2021-10-20 12:30:00")
+let idade = 19
+let palestrantes = ["Pablo", "Derek"]
+let participantes = ["Jessica", "Davi", "Analu"]
+
+let data = prompt("Digite a data: ");
+if (data < hoje){
+    console.log("Data Inválida");
+} else {
+    let idade = prompt("Qual idade do Participante? ");
+    if (idade < 18) {
+        console.log("Cadastro Não Permitido");
+    } else {
+        console.log("Cadastro Realizado");
+        console.log(participantes + palestrantes);
+    }
+}
+
+if (participantes.length < 100) {
+    let numeroParticipantes = prompt("Deseja Cadastar? ");
+} else {
+    console.log("Cadastro Não Será Permitido por ter Excedido o Limite");
+}
